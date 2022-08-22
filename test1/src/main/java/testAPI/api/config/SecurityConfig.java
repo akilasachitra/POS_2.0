58d1eda7a4b4d6b59bcfb193ab724d652884191b
akilasachitra <akilasachitra@gmail.com>
2022-08-22 22:16:07 +0530

    commit 4 / Jpa auth

--- test1/src/main/java/testAPI/api/config/SecurityConfig.java
+++ test1/src/main/java/testAPI/api/config/SecurityConfig.java
@@ -9,15 +9,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import testAPI.api.user.UserService;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -26,87 +20,30 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private DataSource dataSource;
-    @Autowired
     private UserService userService;
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/admin")
-//                .hasRole("ADMIN").antMatchers("/user").hasRole("DOCTOR")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin();
-//    }
 
- @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic().and().formLogin().defaultSuccessUrl("/index.html", true);
     }
 
+
+//    // in-memory
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
-//                .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
-//                .antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
-//                .antMatchers("/delete/**").hasAuthority("ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .logout().permitAll()
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/403");
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("1987"))
+//                .roles("USER").and().withUser("admin").password(passwordEncoder().encode("1987")).roles("ADMIN", "USER");
 //    }
 
-
-
-
-//    //Enable jdbc authentication
-//    @Override
-//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService)
-//                .passwordEncoder(passwordEncoder())// ????????????? role
-//                .and()
-//                .authenticationProvider(authenticationProvider())
-//                .jdbcAuthentication()
-//                .dataSource(dataSource);
-//    }
-
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(userService);
-//        authProvider.setPasswordEncoder(passwordEncoder());
-//        return authProvider;
-//    }
-
-//    @Autowired
-//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled from users where username=?")
-//                .authoritiesByUsernameQuery("select username, role from users where username=?");
-//    }
-//
-    @Override
+    @Override // Jpa-Auth
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("1987"))
-                .roles("USER").and().withUser("admin").password(passwordEncoder().encode("1987")).roles("ADMIN", "USER");
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
-
-
-//    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("user1").password(passwordEncoder().encode("1987")).roles("USER").build();
-//        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("1987")).roles("ADMIN", "USER").build();
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- test1/src/main/java/testAPI/api/user/UserService.java
+++ test1/src/main/java/testAPI/api/user/UserService.java
@@ -1,26 +1,43 @@
 package testAPI.api.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.*;
+
 @Service
+@Slf4j
 public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User byUserName = userRepo.findByUserName(s);
+        log.debug("getting user info from DB");
         if (byUserName != null) {
-            return new UserDTO(byUserName);
-
+            return new org.springframework.security.core.userdetails.User(byUserName.getUserName()
+                    , byUserName.getPassword(), true, true, true, true, getAuthorities(byUserName));
         } else {
             throw new UsernameNotFoundException(s);
         }
-
     }
+
+    public Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+        return authorities;
+    }
+
 }
